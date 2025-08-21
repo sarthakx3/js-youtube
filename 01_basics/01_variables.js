9729e283779abcb3d0c5ca1b0d00dd07d841dd58
sarthakx3 <ssarthaksharma21@gmail.com>
2025-08-21 01:19:58 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1453
+let accountEmail = "xyz@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+accountEmail = "abc@google.com"
+accountPassword = "212121"
+accountCity = "bamglore"
+
+console.log(accountId);
+console.table([accountId , accountEmail ,accountPassword , accountCity]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict;" // treat all JS code as newer version
+
+// alert(3+3) we are using node js and not browser
+
+console.log(3+3); 
+
+console.log("sarthak");
+let name ="sarthak"
+let age = 22
+let isLoggedIn = false
+
+
+//number => 
+//bigint
+//string => " xyz"
+//boolean => true/false
+//null => standalone value
+//undefined => 
+    
+console.log(typeof undefined);
+console.log(typeof undefined);
+
+
