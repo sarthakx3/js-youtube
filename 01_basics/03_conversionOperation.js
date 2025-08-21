83f8cd3408bef6439d80fc7abb60f47f4b6447a5
sarthakx3 <ssarthaksharma21@gmail.com>
2025-08-21 02:34:45 +0000

    converson and comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,50 @@
 let score = 33
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let isLoggedIn = "sarthakk"
+// let isLoggedIn = "sarthakk"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-let somenumber = 33
+// let somenumber = 33
 
-let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(somenumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********operations********** //
+
+let value= 3
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);//power
+// console.log(2/2);//divide
+// console.log(2%2);//remainder
+
+let str1 = "hello"
+let str2 = " sarthak"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+ 2);
+// console.log("1"+ 2);
+// console.log("1"+ 2 + 2);
+// console.log(1+ 2 + "2");
+
+let gameCounter= 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
