a656a0fb858f3bdd8286e0250982cbdad5ffcf9e
sarthakx3 <ssarthaksharma21@gmail.com>
2025-08-21 11:33:02 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,42 @@
+const name = "Sarthak"
+const repoCount = 100;
+
+// console.log(name + repoCount + " value")  not a good method
+
+console.log(`My name is ${name} & my count of repos are ${repoCount}`);
+
+const GameName = new String('ASxSarthakog')
+
+console.log(GameName);
+
+console.log(GameName[5]);
+console.log(GameName.__proto__);
+
+console.log(GameName.toUpperCase());
+
+console.log(GameName.toLowerCase());
+
+console.log(GameName.charAt(2));
+
+console.log(GameName.indexOf('t'));
+
+const newString = GameName.substring(0,4);
+
+// console.log(newString);
+
+const newStringo = GameName.slice(-8,4);
+
+console.log(newStringo);
+
+const newStringOne = "    Sarthak"
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+
+
+
+
