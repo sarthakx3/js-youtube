f1d28d6924cf1b13f7dac7a2cc71f5b4c606d74d
sarthakx3 <ssarthaksharma21@gmail.com>
2025-08-21 01:50:28 +0000

    conversionofdatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = 33
+
+console.log(typeof score);
+
+let isLoggedIn = "sarthakk"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
