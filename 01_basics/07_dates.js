414412003ea5c2e20219a82c7553eb4f0ff6964c
sarthakx3 <ssarthaksharma21@gmail.com>
2025-08-22 14:30:58 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,21 @@
+let myDate = new Date()
+// console.log(myDate.toString())
+
+// console.log(myDate.toDateString())
+
+
+// console.log(Date.now())
+
+let NewDate = new Date()
+
+console.log(NewDate.getDate())
+console.log(NewDate.getMonth())
+
+
+console.log(NewDate.toLocaleString(`default`,{
+    weekday: "long"
+}))
+
+
+
+
