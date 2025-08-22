5666cb9070a21a2e7a75aa8f1b0e33df5b1e7cf3
sarthakx3 <ssarthaksharma21@gmail.com>
2025-08-22 13:50:39 +0000

    numndMaths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,29 @@
+const score = 400
+
+// console.log(score);
+
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length)
+
+const newnumber = 64.9889
+
+// console.log(newnumber.toPrecision(3))
+
+const hundreds = 10000000000;
+
+// console.log(hundreds.toLocaleString(`en-IN`))
+
+// *********************************Maths****************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.PI.toPrecision(4))
+// console.log(Math.round(4.5))
+// console.log(Math.floor(4.2))
+
+// console.log(Math.sqrt(5).toPrecision(5))
+
+console.log(((Math.random()*10)+1).toPrecision(1))
