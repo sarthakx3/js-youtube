d79adb2d535c9b37c80d35750b0f2e180aab55af
sarthakx3 <ssarthaksharma21@gmail.com>
2025-08-23 17:16:45 +0000

    array2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,27 @@
+//arrays
+
+const marvel_heroes = ["thor","ironman","spoidermon"]
+const dc_heroes = ["alpha","beta","gamma"]
+
+// marvel_heroes.push(dc_heroes.join())
+
+// console.log(marvel_heroes);
+// const allHeros = marvel_heroes.concat(dc_heroes)
+console.log(marvel_heroes.concat(dc_heroes))
+
+console.log(...marvel_heroes,...dc_heroes)
+
+let newar = "sarthak"
+
+// console.log(Array.from(newar))
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
