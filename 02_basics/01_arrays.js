2f229c9162a637601f20dd54c5e68b19bccabff4
sarthakx3 <ssarthaksharma21@gmail.com>
2025-08-23 15:02:17 +0000

    array1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,31 @@
+//arrays
+
+const arr= [0,1,2,3,4,5]
+
+// console.log(arr)
+// const arr1 = ["alpha" , "beta"]
+
+// console.log(arr1.length)
+
+// arr.push(5)
+// console.log(arr)
+// arr.push(8)
+// console.log(arr[6])
+
+//slice,splice
+
+const newarray = [0,1,2,3,4,5]
+console.log(newarray);
+
+const nya1 = newarray.slice(1,4)
+// console.log(nya1)
+// console.log(newarray);
+
+console.log(newarray.reverse());
+
+const nya2 = (newarray.reverse())
+console.log(nya2.splice(1,3))
+console.log(newarray);
+
+
+
